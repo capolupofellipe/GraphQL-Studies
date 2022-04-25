df73b9460a97960edb3a7f511ab027dface16ba3
Fellipe Capolupo <fellipe.capolupo@kinvo.com.br>
2022-04-25 13:20:19 -0300

    FEAT: Implements sub queries

--- WebApi/Types/MenuType.cs
+++ WebApi/Types/MenuType.cs
@@ -1,15 +1,17 @@
 ﻿using GraphQL.Types;
+using WebApi.Interfaces;
 using WebApi.Models;
 
 namespace WebApi.Types
 {
     public class MenuType : ObjectGraphType<Menu>
     {
-        public MenuType()
+        public MenuType(ISubMenuService subMenuService)
         {
             Field(x => x.Id);
             Field(x => x.Name);
             Field(x => x.ImageUrl);
+            Field<ListGraphType<SubMenuType>>("subMenus", resolve: context => { return subMenuService.GetByMenu(context.Source.Id); });
         }
     }
 }
